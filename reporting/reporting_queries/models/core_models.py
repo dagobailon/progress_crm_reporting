b326a50bcd5ec89ef261a5886fa9878cdb832d31
dagobailon <dago@codeforprogress.org>
2014-07-30 14:25:15 -0400

    Fixed models

---
+++ reporting/reporting_queries/models/core_models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Organization(models.Model):
+	name = models.CharField(max_length=255)
+	abbreviation = models.CharField(max_length=31)
+	parent = models.ForeignKey('Organization', related_name='children')
+	address = models.ForeignKey('PostalAddress')
+
+	created_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+	class Meta:
+    	app_label = 'reporting_queries'
+    	db_table = 'reporting_queries_Organization'
+
+	def __unicode__(self):
+		return self.name
