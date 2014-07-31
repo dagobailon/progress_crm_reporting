3d6c51bd69362effb068087c2c7a0209613d8813
dagobailon <dago@codeforprogress.org>
2014-07-31 10:46:07 -0400

    fixed the init file

--- reporting/reporting_queries/__init__.py
+++ reporting/reporting_queries/__init__.py
@@ -1,10 +1,10 @@
-from person_models import *
-from event_models import *
-from donation_models import *
-from list_models import *
-from adapter_models import *
-from form_models import *
-from queries_models import *
+# from person_models import *
+# from event_models import *
+# from donation_models import *
+# from list_models import *
+# from adapter_models import *
+# from form_models import *
+# from queries_models import *
 
 #from reporting_queries.models.adapter_models import SyncOperations
 #from reporting_queries.models.core_models import Organization
