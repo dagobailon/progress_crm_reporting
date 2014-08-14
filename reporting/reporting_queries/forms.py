ee055f5361f9ba39c35fe225449ca47c7a5dd3bc
dagobailon <dago@codeforprogress.org>
2014-08-14 14:07:02 -0400

    commented code and added addquery user interface feature

--- reporting/reporting_queries/forms.py
+++ reporting/reporting_queries/forms.py
@@ -1,8 +1,11 @@
+#Section 1: Imports
 from django import forms
 from django.forms import ModelForm
 from reporting_queries.models import Query
 
+#Section 2: Forms
 class AddQueryForm(forms.Form):
-    query = forms.CharField(label = 'query', max_length=500)  
-    name = forms.CharField(label = 'name', max_length=120)
-    description = forms.CharField(label = 'description')
+#form to create the empty fields for the Query model. 
+    query = forms.CharField(label = 'query', max_length=500, required=True)  
+    name = forms.CharField(label = 'name', max_length=120, required=True)
+    description = forms.CharField(label = 'description', required=True)
