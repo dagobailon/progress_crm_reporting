b7da717607cb7372107fd619e723bdd950e3d1fc
dagobailon <dago@codeforprogress.org>
2014-08-14 18:17:36 -0400

    added dashboard module

--- progress_crm_reporting/dashboard_module.py
+++ progress_crm_reporting/dashboard_module.py
@@ -1,1 +1,10 @@
 from progress_crm.plugins import CRMDashboardModule
+
+class CRMReportingModule(CRMDashboardModule):
+    title = 'Reporting'
+
+    def is_empty():
+        return False 
+
+    def init_with_context(self, context):
+        context['queries'] = Query.objects.all()
